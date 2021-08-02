8d7d4152e0dd4f8f374369eefa5183aa39e3d766
oldataraxia <aqwanghaoxuan@163.com>
2021-08-02 23:40:49 +0800

    feat:add string parsing

--- leptjson.c
+++ leptjson.c
@@ -3,12 +3,40 @@
 #include <stdlib.h>  /* NULL, strtod() */
 #include <math.h>
 #include <errno.h>
+#include <string.h>
+
+#ifndef LEPT_PARSE_STACK_INIT_SIZE
+#define LEPT_PARSE_STACK_INIT_SIZE 256
+#endif
 
 #define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
+#define ISDIGIT(ch)         ((ch) >= '0' && (ch) <= '9')
+#define ISDIGIT1TO9(ch)     ((ch) >= '1' && (ch) <= '9')
+#define PUTC(c, ch)         do{ *(char*)lept_context_push(c, sizeof(char)) = (ch); }while(0)
 
 typedef struct {
     const char* json;
+    char* stack;
+    size_t size, top;
 }lept_context;
+
+static void* lept_context_push(lept_context* c, size_t size){
+    void* ret;
+    assert(size > 0);
+    if(c->top + size >= c->size){
+        if(c->size == 0) c->size = LEPT_PARSE_STACK_INIT_SIZE;
+        while(c->top + size >= c->size) c->size += c->size >> 1; /*c->Size * 1.5*/
+        c->stack = (char*)realloc(c->stack, c->size);
+    }
+    ret = c->stack + c->top;
+    c->top += size;
+    return ret;
+}
+
+static void* lept_context_pop(lept_context* c, size_t size){
+    assert(c->top >= size);
+    return c->stack + (c->top -= size);
+}
 
 /*
     删除前面的 whitespace
@@ -32,9 +60,6 @@ static int lept_parse_literal(lept_context* c, lept_value* v, const char* litera
     v->type = type;
     return LEPT_PARSE_OK;
 }
-
-#define ISDIGIT(ch)         ((ch) >= '0' && (ch) <= '9')
-#define ISDIGIT1TO9(ch)     ((ch) >= '1' && (ch) <= '9')
 
 static int lept_parse_number(lept_context* c, lept_value* v){
     /* 解析数字 */
@@ -64,13 +89,56 @@ static int lept_parse_number(lept_context* c, lept_value* v){
         for(p++; ISDIGIT(*p); p++);
     }
     errno = 0;
-    v->n = strtod(c->json, NULL);
-    if(errno = ERANGE && (v->n == HUGE_VAL || v->n == -HUGE_VAL)){
+    v->u.n = strtod(c->json, NULL);
+    if(errno = ERANGE && (v->u.n == HUGE_VAL || v->u.n == -HUGE_VAL)){
         return LEPT_PARSE_NUMBER_TOO_BIG;
     }
     c->json = p;
     v->type = LEPT_NUMBER;
     return LEPT_PARSE_OK;
+}
+
+char escapeSuffix[8] = {'\"', '\\', '/', 'b', 'f', 'n', 'r', 't'};
+char escape[8] = {'\"', '\\', '/', '\b', '\f', '\n', '\r', '\t'};
+
+static int lept_parse_string(lept_context* c, lept_value* v){
+    size_t head = c->top, len, i;
+    int flag = 0;
+    const char* p;
+    EXPECT(c, '\"'); /* 在这里c->json++了 */
+    p = c->json;
+    while(1){
+        char ch = *p++;
+        switch(ch){
+            case '\"':
+                len = c->top - head;
+                lept_set_string(v, (const char*)lept_context_pop(c, len), len);
+                c->json = p;
+                return LEPT_PARSE_OK;\
+            case '\0':
+                c->top = head;
+                return LEPT_PARSE_MISS_QUOTATION_MARK;
+            case '\\':
+                flag = 0;
+                for(i = 0; i < 8; i++){
+                    if(escapeSuffix[i] == *p){
+                        PUTC(c, escape[i]);
+                        p++;
+                        flag = 1;
+                        break;
+                    }
+                }
+                if(flag) break;
+                c->top = head;
+                return LEPT_PARSE_INVALID_STRING_ESCAPE;
+            default: 
+                if ((unsigned char)ch < 0x20) { 
+                    c->top = head;
+                    return LEPT_PARSE_INVALID_STRING_CHAR;
+                }
+                PUTC(c, ch);
+        }
+    }
 }
 
 /*
@@ -83,6 +151,7 @@ static int lept_parse_value(lept_context* c, lept_value* v) {
         case 't': return lept_parse_literal(c, v, "true", LEPT_TRUE);
         case 'f': return lept_parse_literal(c, v, "false", LEPT_FALSE);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
+        case '"': return lept_parse_string(c, v);
         default:   return lept_parse_number(c, v);
     }
 }
@@ -99,7 +168,9 @@ int lept_parse(lept_value* v, const char* json) {
     int res;
     assert(v != NULL);
     c.json = json;
-    v->type = LEPT_NULL;
+    c.stack = NULL;
+    c.size = c.top = 0;
+    lept_init(v);
     lept_parse_whitespace(&c);
     /* lept_parse_value写入新的值 */
     if((res = lept_parse_value(&c, v)) == LEPT_PARSE_OK){
@@ -109,6 +180,8 @@ int lept_parse(lept_value* v, const char* json) {
             res = LEPT_PARSE_ROOT_NOT_SINGULAR;
         }
     }
+    assert(c.top == 0);
+    free(c.stack);
     return res;
 }
 
@@ -117,7 +190,54 @@ lept_type lept_get_type(const lept_value* v) {
     return v->type;
 }
 
+void lept_free(lept_value* v){
+    assert(v != NULL);
+    if(v->type == LEPT_STRING){
+        free(v->u.s.s);
+    }
+    v->type = LEPT_NULL;
+}
+
+int lept_get_boolean(const lept_value* v){
+    assert(v != NULL && (v->type == LEPT_TRUE || v->type == LEPT_FALSE));
+    return v->type == LEPT_TRUE ? 1 : 0;
+}
+
+void lept_set_boolean(lept_value* v, int b){
+    assert(v != NULL);
+    lept_set_null(v);
+    if(b == 1) v->type = LEPT_TRUE;
+    else v->type = LEPT_FALSE;
+}
+
 double lept_get_number(const lept_value* v){
     assert(v != NULL && v->type == LEPT_NUMBER);
-    return v->n;
+    return v->u.n;
+}
+
+void lept_set_number(lept_value* v, double n){
+    assert(v != NULL);
+    lept_set_null(v);
+    v->u.n = n;
+    v->type = LEPT_NUMBER;
+}
+
+const char* lept_get_string(const lept_value* v){
+    assert(v != NULL && v->type == LEPT_STRING);
+    return v->u.s.s;
+}
+
+size_t lept_get_string_length(const lept_value* v){
+    assert(v != NULL && v->type == LEPT_STRING);
+    return v->u.s.len;
+}
+
+void lept_set_string(lept_value* v, const char* s, size_t len){
+    assert(v != NULL && (s != NULL || len == 0));
+    lept_free(v);
+    v->u.s.s = (char*)malloc(len + 1);
+    memcpy(v->u.s.s, s, len);
+    v->u.s.s[len] = '\0';
+    v->u.s.len = len;
+    v->type = LEPT_STRING;
 }
--- leptjson.h
+++ leptjson.h
@@ -1,10 +1,15 @@
 #ifndef LEPTJSON_H__
 #define LEPTJSON_H__
 
+#include<stddef.h>
+
 typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;
 
 typedef struct {
-    double n;
+    union{
+        struct {char* s; size_t len;}s;
+        double n;
+    }u;
     lept_type type;
 }lept_value;
 
@@ -13,13 +18,30 @@ enum {
     LEPT_PARSE_EXPECT_VALUE,
     LEPT_PARSE_INVALID_VALUE,
     LEPT_PARSE_ROOT_NOT_SINGULAR,
-    LEPT_PARSE_NUMBER_TOO_BIG
+    LEPT_PARSE_NUMBER_TOO_BIG,
+    LEPT_PARSE_MISS_QUOTATION_MARK,
+    LEPT_PARSE_INVALID_STRING_ESCAPE,
+    LEPT_PARSE_INVALID_STRING_CHAR
 };
+
+#define lept_init(v) do { (v)->type = LEPT_NULL; } while(0)
 
 int lept_parse(lept_value* v, const char* json);
 
+void lept_free(lept_value* v);
+
 lept_type lept_get_type(const lept_value* v);
 
+#define lept_set_null(v) lept_free(v)
+
+int lept_get_boolean(const lept_value* v);
+void lept_set_boolean(lept_value* v, int n);
+
 double lept_get_number(const lept_value* v);
+void lept_set_number(lept_value* v, double n);
+
+const char* lept_get_string(const lept_value* v);
+size_t lept_get_string_length(const lept_value* v);
+void lept_set_string(lept_value* v, const char* s, size_t len);
 
 #endif /* LEPTJSON_H__ */
